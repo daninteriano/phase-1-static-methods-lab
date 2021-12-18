7953412954ab4a0c079c34f4ab816f55b4a80789
Daniel Interiano <dannyboyint@gmail.com>
2021-12-17 21:49:28 -0500

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Formatter {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   static sanitize(str){
-    return str.replace(/[^A-Za-z0-9']+/g, "");
+    return str.replace(/[^A-Za-z0-9'- ]+/g, "");
     
   }
   static titleize(string){
