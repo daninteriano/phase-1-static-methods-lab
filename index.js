e7904dbd243f489a7cd223207f3d349d27efbc60
Daniel Interiano <dannyboyint@gmail.com>
2021-12-17 21:34:06 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9']+/g, "");
+    
+  }
+  static titleize(string){
+   return string.split(' ').map(function(word){
+      return word.charAt(0).toUpperCase() + word.slice(1)}
+      ).join(" ")
+  }
 }
