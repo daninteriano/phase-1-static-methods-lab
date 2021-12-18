cf9232756c060c885d8fbd54715227c312870756
Daniel Interiano <dannyboyint@gmail.com>
2021-12-17 22:09:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ class Formatter {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
   static sanitize(str){
-    return str.replace(/[^A-Za-z0-9'- ]+/g, "");
+    return str.replace(/[^A-Za-z0-9 '-]+/g, "");
     
   }
   static titleize(string){
-   return string.split(' ').map(function(word){
-      return word.charAt(0).toUpperCase() + word.slice(1)}
-      ).join(" ")
+    let hidden = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the']
+   return string.split(' ').map(function(word, idx){
+     if(idx === 0 ){
+       return word.charAt(0).toUpperCase() + word.slice(1)
+     }
+     if(hidden.includes(word)){
+       return word;
+     } else {
+       return word.charAt(0).toUpperCase() + word.slice(1)}
+   }).join(" ") 
   }
 }
